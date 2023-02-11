1e08cf147070c98777e30445aed3b87017f46cf4
SeonAhP <106813317+SeonAhP@users.noreply.github.com>
2023-02-11 16:01:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hi ! 🖐🖐🖐',
-    introDesc: `대답 좀 해주세요!`,
+    introDesc: `Em có điều này muốn hỏi`,
     btnIntro: 'OK!',
-    title: '일요일 나랑 영화 보러 갈래?',
-    desc: '대답하지 않거나 나간다는 것은 동의한다는 뜻! ',
-    btnYes: 'Yesss',
-    btnNo: '💔 No! 💔 ',
-    question:'범죄도시2 or 쥬라기 월드?',
-    reasonPlaceholder: 'Please choose one',
+    title: 'Anh la cua em duoc khongg?',
+    desc: 'Anh không trả lời hoặc thoát ra nghĩa là đồng ý đóo ',
+    btnYes: 'Okiii',
+    btnNo: '💔 Hong! 💔 ',
+    question:'Tại sao anh thích em vậy?',
+    reasonPlaceholder: 'Lý do',
     btnReply: 'Send',
-    reply: '쥬라기 월드 보자!',
-    mess: 'I know you will choose Jurassic World',
-    messDesc: '💬💬 Send me messenger to discuss! 💬💬',
+    reply: 'Vì em cute dịu dàng chinh đẹppp',
+    mess: 'Em biết mà, iu anhhhhh',
+    messDesc: 'Nhắn tin cho em điii',
     btnAccept: 'Ok!',
     messLink: 'https://www.facebook.com/luong.ly.92'
 }
